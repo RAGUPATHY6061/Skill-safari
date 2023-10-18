78e0397c5d0ff02f996fbf5f5e775997712cc05d
RAGUPATHY6061 <ragupathyragav.n@gmail.com>
2023-10-18 21:48:44 +0530

    Commit

--- flixx-app-theme/js/profile.js
+++ flixx-app-theme/js/profile.js
@@ -12,19 +12,36 @@ const firebaseConfig = {
     messagingSenderId: "781259805947",
     appId: "1:781259805947:web:b257085265cdee71c8cc5e"
 };
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase();
 const auth = getAuth(app);
-
 const storedUserId = localStorage.getItem('userId');
 const userId = storedUserId;
 
 const profileName = document.getElementById("profile-name");
 const profileEmail = document.getElementById("profile-email");
 const profileGender = document.getElementById("profile-gender");
+const profileInterests = document.getElementById("profile-interests");
 const editBtn = document.getElementById('edit-btn');
 const saveBtn = document.getElementById('save-btn');
 const editContainer = document.getElementById('edit-container');
+
+const displayUserData = (data) => {
+    if (data) {
+        profileName.textContent = data.username;
+        profileEmail.textContent = data.email;
+        profileGender.textContent = data.gender;
+
+        if (data.interests && data.interests.length > 0) {
+            profileInterests.textContent = data.interests.join(', ');
+        } else {
+            profileInterests.textContent = 'None';
+        }
+    } else {
+        console.log("No data available for this user.");
+    }
+};
 
 editBtn.addEventListener('click', () => {
     editContainer.style.display = 'block';
@@ -35,31 +52,38 @@ editBtn.addEventListener('click', () => {
     editBtn.style.display = 'none';
     saveBtn.style.display = 'block';
 });
-
-saveBtn.addEventListener('click', () => {
+saveBtn.addEventListener('click', async () => {
     const newName = document.getElementById('edit-name').value;
     const newEmail = document.getElementById('edit-email').value;
     const newGender = document.getElementById('edit-gender').value;
+
+    const interests = document.querySelectorAll('input[name="interest"]:checked');
+    const selectedInterests = Array.from(interests).map((item) => item.value);
+
     profileName.textContent = newName;
     profileEmail.textContent = newEmail;
     profileGender.textContent = newGender;
+    profileInterests.textContent = selectedInterests.join(', ');
 
-    set(ref(database, 'users/' + userId), {
-        username: newName,
-        email: newEmail,
-        gender: newGender
-    });
+    try {
+        await set(ref(database, 'users/' + userId), {
+            username: newName,
+            email: newEmail,
+            gender: newGender,
+            interests: selectedInterests
+        });
 
-    const user = auth.currentUser;
-    updateProfile(user, {
-        displayName: newName,
-        email: newEmail,
-        gender: newGender,
-    }).then(() => {
-        console.log("User profile updated successfully.");
-    }).catch((error) => {
-        console.log("Error updating user profile:", error);
-    });
+        const user = auth.currentUser;
+        await updateProfile(user, {
+            displayName: newName,
+            email: newEmail,
+            gender: newGender
+        });
+
+       showSuccess("User profile updated successfully.");
+    } catch (error) {
+        showAlert("Error updating user profile:", error);
+    }
 
     editContainer.style.display = 'none';
     document.getElementById('profile-container').style.display = 'block';
@@ -70,11 +94,25 @@ saveBtn.addEventListener('click', () => {
 const profileRef = ref(database, 'users/' + userId);
 onValue(profileRef, (snapshot) => {
     const profileData = snapshot.val();
-    if (profileData) {
-        profileName.textContent = profileData.username;
-        profileEmail.textContent = profileData.email;
-        profileGender.textContent = profileData.gender;
-    } else {
-        console.log("No data available for this user.");
-    }
+    displayUserData(profileData);
 });
+function showAlert(message, className = 'error') {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+    setTimeout(() => alertEl.remove(), 3000);
+}
+
+function showSuccess(message, className = 'success') {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+    setTimeout(() => alertEl.remove(), 1000);
+}
+
+
+
+
+
--- flixx-app-theme/js/signup.js
+++ flixx-app-theme/js/signup.js
@@ -11,95 +11,119 @@
   messagingSenderId: "781259805947",
   appId: "1:781259805947:web:b257085265cdee71c8cc5e"
 };
- 
- const app = initializeApp(firebaseConfig);
- 
- const signupBtn = document.getElementById("signup-btn");
- const nameField = document.getElementById("name");
- const signupEmail = document.getElementById("signup-email");
- const signupPassword = document.getElementById("signup-password");
- const confirmPassword = document.getElementById("confirm-password");
- const genderField = document.getElementById("gender");
- 
- const auth = getAuth();
- const database = getDatabase();
- 
- signupBtn.addEventListener("click", () => {
-  const name = nameField.value;
-  const email = signupEmail.value;
-  const password = signupPassword.value;
-  const confirm = confirmPassword.value;
-  const gender = genderField.value;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase();
 
-  if (password !== confirm) {
-      showAlert("Passwords do not match");
-      return;
-  }
+const signupBtn = document.getElementById("signup-btn");
+const nameField = document.getElementById("name");
+const signupEmail = document.getElementById("signup-email");
+const signupPassword = document.getElementById("signup-password");
+const confirmPassword = document.getElementById("confirm-password");
+const genderField = document.getElementById("gender");
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        console.log("User signed up: " + user.uid);
-        localStorage.setItem('userId', user.uid); 
-        return saveUserData(user.uid, name, email, gender,password);
-    })
-    .then(() => {
-        showSuccess("You are Successfully signed up")
-        setTimeout(() =>window.location.href = "index.html" , 1000);
-        clearFields();
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        if(errorCode === "auth/invalid-email"){
-            showAlert("Enter the email and password");
-        }
-        else if(errorCode === "auth/missing-password"){
-            showAlert("Enter the password");
+signupBtn.addEventListener("click", () => {
+    const name = nameField.value;
+    const email = signupEmail.value;
+    const password = signupPassword.value;
+    const confirm = confirmPassword.value;
+    const gender = genderField.value;
+    if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
+        showAlert("Please enter all required fields");
+        return;
+    }
 
-        }
-        else if(errorCode === "auth/email-already-in-use"){
-            showAlert("Email already exits");
-        }
-        else if(errorCode === "auth/weak-password"){
-            showAlert("Enter a strong password");
-        }
-        console.log(errorCode);
-        clearFields();
+    if (password !== confirm) {
+        showAlert("Passwords do not match");
+        return;
+    }
+    let interests = [];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    const radios = document.querySelectorAll('input[type="radio"]:checked');
 
+    if (checkboxes.length === 0 || radios.length === 0) {
+        showAlert("Please select your interests");
+        return;
+    }
+
+    checkboxes.forEach(checkbox => {
+        interests.push(checkbox.value);
     });
+
+    radios.forEach(radio => {
+        interests.push(radio.value);
+    });
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("User signed up: " + user.uid);
+            localStorage.setItem('userId', user.uid);
+            return saveUserData(user.uid, name, email, gender, password, interests);
+        })
+        .then(() => {
+            showSuccess("You are Successfully signed up");
+            setTimeout(() => (window.location.href = "index.html"), 1000);
+            clearFields();
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            let errorMessage = error.message;
+            if (errorCode === "auth/invalid-email") {
+                errorMessage = "Enter a valid email address";
+            } else if (errorCode === "auth/missing-password") {
+                errorMessage = "Enter the password";
+            } else if (errorCode === "auth/email-already-in-use") {
+                errorMessage = "Email already exists";
+            } else if (errorCode === "auth/weak-password") {
+                errorMessage = "Enter a strong password";
+            }
+            showAlert(errorMessage);
+            clearFields();
+        });
 });
 
-function saveUserData(userId, name, email, gender,password) {
-  return set(ref(database, 'users/' + userId), {
-      username: name,
-      email: email,
-      gender: gender,
-      password:password
-  });
+
+function saveUserData(userId, name, email, gender, password, interests) {
+    return set(ref(database, 'users/' + userId), {
+        username: name,
+        email: email,
+        gender: gender,
+        password: password,
+        interests: interests
+    });
 }
 
- function clearFields() {
-     nameField.value = '';
-     signupEmail.value = '';
-     signupPassword.value = '';
-     confirmPassword.value = '';
-     genderField.value = 'Male'; 
- }
- function showAlert(message, className = 'error') {
+function clearFields() {
+    nameField.value = '';
+    signupEmail.value = '';
+    signupPassword.value = '';
+    confirmPassword.value = '';
+    genderField.value = 'Male';
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+
+    const radios = document.querySelectorAll('input[type="radio"]:checked');
+    radios.forEach((radio) => {
+        radio.checked = false;
+    });
+}
+
+function showAlert(message, className = 'error') {
     const alertEl = document.createElement('div');
     alertEl.classList.add('alert', className);
     alertEl.appendChild(document.createTextNode(message));
     document.querySelector('#alert').appendChild(alertEl);
     setTimeout(() => alertEl.remove(), 3000);
-  }
+}
 
-  function showSuccess(message, className = 'success') {
+function showSuccess(message, className = 'success') {
     const alertEl = document.createElement('div');
     alertEl.classList.add('alert', className);
     alertEl.appendChild(document.createTextNode(message));
     document.querySelector('#alert').appendChild(alertEl);
     setTimeout(() => alertEl.remove(), 1000);
-  }
- 
-
+}
